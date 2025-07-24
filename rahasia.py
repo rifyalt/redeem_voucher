d4e98991451dbbc6b36ee0c1995d30b296a39b3c
vial <60603588+rifyalt@users.noreply.github.com>
2025-07-24 13:38:44 +0700

    Innitial Commit

---
+++ rahasia.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["google"], scope)
+client = gspread.authorize(creds)
